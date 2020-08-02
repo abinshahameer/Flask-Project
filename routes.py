3c427fce5ab1b3c87e1d1dc7255f16d282ef200e
Abinshah Ameer <62852309+abinshahameer@users.noreply.github.com>
2020-08-02 21:43:34 +0530

    Add files via upload
    Final

--- routes.py
+++ routes.py
@@ -1,17 +1,58 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, url_for, flash, get_flashed_messages, redirect, request
+from datetime import datetime
+
+import models
 import forms
 
-
 @app.route('/')
+@app.route('/index')
 def index():
-    return render_template('index.html', current_title='Custom Title')
+    tasks = models.Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
 
-@app.route('/about', methods=['GET', 'POST'])
-def about():
+@app.route('/add', methods=['GET', 'POST'])
+def add():
     form = forms.AddTaskForm()
     if form.validate_on_submit():
-        print("Submited title", form.title.data)
-        return render_template('about.html', form=form, title=form.title.data)
-    return render_template('about.html', form=form)
+        task = models.Task(title=form.title.data, date=datetime.utcnow())
+        db.session.add(task)
+        db.session.commit()
+        flash('Task added')
+        return redirect(url_for('index'))
+    return render_template('add.html', form=form)
+
+
+@app.route('/edit/<int:task_id>', methods=['GET', 'POST'])
+def edit(task_id):
+    form = forms.AddTaskForm()
+    task = models.Task.query.get(task_id)
+    print(task)
+    if task:
+        if form.validate_on_submit():
+            task.title = form.title.data
+            task.date = datetime.utcnow()
+            db.session.commit()
+            flash('Task updated')
+            return redirect(url_for('index'))
+        form.title.data = task.title
+        return render_template('edit.html', form=form, task_id=task_id)
+    flash(f'Task with id {task_id} does not exit')
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<int:task_id>', methods=['GET', 'POST'])
+def delete(task_id):
+    form = forms.DeleteTaskForm()
+    task = models.Task.query.get(task_id)
+    if task:
+        if form.validate_on_submit():
+            if form.submit.data:
+                db.session.delete(task)
+                db.session.commit()
+                flash('Task deleted')
+            return redirect(url_for('index'))
+        return render_template('delete.html', form=form, task_id=task_id, title=task.title)
+    flash(f'Task with id {task_id} does not exit')
+    return redirect(url_for('index'))
